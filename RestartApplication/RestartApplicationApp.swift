48357feccb72c46c2dcca0c29a11e8d6ba5d13de
Alexander Adegbenro <adegbenroalexander15@gmail.com>
2022-02-01 19:49:03 -0500

    Initial Commit

---
+++ RestartApplication/RestartApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApplicationApp.swift
+//  RestartApplication
+//
+//  Created by Field Employee on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
