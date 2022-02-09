1e2a090a41994d812c745936fedc0940352968bc
Alexander Adegbenro <adegbenroalexander15@gmail.com>
2022-02-08 20:36:02 -0500

    Finished Building Ui

--- RestartApplication/Screens/OnboardingView.swift
+++ RestartApplication/Screens/OnboardingView.swift
@@ -2,7 +2,7 @@
 //  OnboardingView.swift
 //  RestartApplication
 //
-//  Created by Field Employee on 2/7/22.
+//  Created by Alexander Adegbenro on 2/7/22.
 //
 
 import SwiftUI
@@ -15,16 +15,105 @@ struct OnboardingView: View {
     // MARK: - BODY
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
-            
-            Button(action: {
-                isOnboardingViewActive = false
-            }){
-                Text("Start")
-            }
-        } //: VSTACK
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack(spacing: 20) {
+                
+                //MARK: - HEADER
+                
+                Spacer()
+                VStack(spacing: 0 ){
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    Text("""
+                            Its's not how much we give but how much love we put into giving.
+                            """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
+                } //: HEADER
+                
+                //MARK: - CENTER
+                
+                ZStack {
+                    CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                    //: ZSTACK
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFill()
+                    Spacer()
+                    
+                } //: CENTER
+                
+                
+                
+                
+                //MARK: - FOOTER
+                
+                ZStack {
+                    // PARTS OF THE custom button
+                    
+                    // 1. background(static)
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // 2. call-to-action (ststic)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    
+                    // 3. capsule (synamic width))
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    // 4. circle (Draggable)
+                    
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        
+                        Spacer()
+                    }
+                    
+                    
+                }  //: FOOTER
+                .frame(height: 80, alignment: .center)
+                .padding()
+            } //: VSTACK
+        } //: ZSTACK
     }
 }
 
---
+++ RestartApplication/Views/CircleGroupView.swift
@@ -0,0 +1,36 @@
+//
+//  CircleGroupView.swift
+//  RestartApplication
+//
+//  Created by Field Employee on 2/8/22.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    // MARK: - PROPERTY
+    
+    @State var ShapeColor: Color
+    @State var ShapeOpacity: Double
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        } //: ZSTACK
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color(.brown)
+                .ignoresSafeArea(.all, edges: .all)
+            CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+        }
+    }
+}
